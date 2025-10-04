961d21a4dc7f4eca205da1c75ab2dd640781dfaf
666hackeranonim <mestniyhuntercsgo@gmail.com>
2025-10-04 12:36:51 +0000

    lab5

--- sites/skuit/www/index.php
+++ sites/skuit/www/index.php
@@ -1,5 +1,5 @@
 <?
-include_once '/workspaces/php-course-template/sites/skuit/www/lab4/header.php';
-include_once '/workspaces/php-course-template/sites/skuit/www/lab4/footer.php';
-include_once '/workspaces/php-course-template/sites/skuit/www/lab4/hobits.php';
+include_once './lab5/header.php';
+include_once './lab5/main.php';
+include_once './lab5/footer.php';
 ?>
